aac6c04fc625bf56ac0cae25ac836ecf142f8665
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-07-09 15:42:37 +0200

    feat: add new component main

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script>
 import AppHeader from '../src/components/AppHeader.vue'
+import AppMain from '../src/components/AppMain.vue'
 
 export default {
-  name: "App",
   components: {
-    AppHeader
+    AppHeader,
+    AppMain,
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <AppMain />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "AppMain",
+}
+</script>
+
+<template>
+    <main>
+        <div class="container"></div>
+    </main>
+</template>
+    
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+main {
+    background-color: $primary;
+    // DEBUG
+    height: calc(100vh - 100px)  
+}
+
+</style>
